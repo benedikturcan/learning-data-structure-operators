6968884de72440b0cf0d3ae70bdb67a4eaa53986
benedikturcan <benedikt_urcan@hotmail.de>
2023-02-15 09:39:20 +0100

    added more traning code

--- script.js
+++ script.js
@@ -35,41 +35,90 @@ const restaurant = {
       `Order received ${time}, this customer ordered meal ${this.mainMenu[mainIndex]} and ${this.starterMenu[starterIndex]} and the delivery address will be ${address}`
     );
   },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your deliciouse pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
 };
 
-restaurant.orderDelivery({
-  time: '22:30',
-  address: 'Pannwitzallee 10',
-  mainIndex: 2,
-  starterIndex: 2,
-});
+const arr = [7, 8, 9];
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArr);
 
-const { name, openingHours, categories } = restaurant;
-console.log(name, openingHours, categories);
+const newArr = [1, 2, ...arr];
+console.log(newArr);
 
-const {
-  name: restaurantName,
-  openingHours: hours,
-  categories: tags,
-} = restaurant;
-console.log(restaurantName, hours, tags);
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+console.log(newMenu);
 
-const { menu = [], starterMenu: starters = [] } = restaurant;
-console.log(menu, starters);
+// Copy arr
+const mainMenuCopy = [...restaurant.mainMenu];
 
-//Mutating  variables
-let a = 111;
-let b = 999;
-const obj = { a: 23, b: 7, c: 14 };
+//join Arr
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu);
 
-({ a, b } = obj);
-console.log(a, b);
+// Interables are arrays, strings, maps sets
+const str = 'Benedikt';
+const letters = [...str, '', 'S.'];
+console.log(letters);
 
-// nested ojb
-const {
-  fri: { open, close },
-} = openingHours;
-console.log(open, close);
+// const ingredients = [
+//   prompt("Let's make pasta! Ingredient 1"),
+//   prompt("Let's make pasta! Ingredient 2"),
+//   prompt("Let's make pasta! Ingredient 3"),
+// ];
+// console.log(ingredients);
+
+// restaurant.orderPasta(...ingredients);
+
+// Objects
+const newRestaurant = {
+  founndingYear: 1998,
+  ...restaurant,
+  founder: 'Benedikt',
+};
+console.log(newRestaurant);
+
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = 'Ristaurante Roma';
+console.log(restaurantCopy);
+
+// restaurant.orderDelivery({
+//   time: '22:30',
+//   address: 'Pannwitzallee 10',
+//   mainIndex: 2,
+//   starterIndex: 2,
+// });
+
+// const { name, openingHours, categories } = restaurant;
+// console.log(name, openingHours, categories);
+
+// const {
+//   name: restaurantName,
+//   openingHours: hours,
+//   categories: tags,
+// } = restaurant;
+// console.log(restaurantName, hours, tags);
+
+// const { menu = [], starterMenu: starters = [] } = restaurant;
+// console.log(menu, starters);
+
+// //Mutating  variables
+// let a = 111;
+// let b = 999;
+// const obj = { a: 23, b: 7, c: 14 };
+
+// ({ a, b } = obj);
+// console.log(a, b);
+
+// // nested ojb
+// const {
+//   fri: { open, close },
+// } = openingHours;
+// console.log(open, close);
 
 // const arr = [2, 3, 4];
 // const a = arr[0];
